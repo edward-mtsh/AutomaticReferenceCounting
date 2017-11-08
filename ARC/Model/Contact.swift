90f90f598d3cf777191fa5991ce8d19c86d038f0
eddie~mtsh <eddie.magents@gmail.com>
2017-11-08 10:31:50 +0200

    initial commit

---
+++ ARC/Model/Contact.swift
@@ -0,0 +1,13 @@
+//
+//  Contact.swift
+//  ARC
+//
+//  Created by Edward Mtshweni on 2017/11/08.
+//  Copyright © 2017 Edward Mtshweni. All rights reserved.
+//
+
+import UIKit
+
+class Contact: NSObject {
+
+}
